2eff887f15544d3ff0af876cdc55f26551168def
Wilder Cossio <w.cossio.r@gmail.com>
2024-03-11 20:30:32 -0400

    creating a top banner and importing a video

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.css'; 
+import video from './assets/video.mp4';
 
 function App() {
   return (
-    <div className="App">
-     <div>Hola mundo</div>
+    <div>
+     <div className='banner'>
+      <video src={video} autoPlay loop muted></video>
+     </div>
+      <h1 className='logo'>TimeTracker</h1>
     </div>
   );
 }
