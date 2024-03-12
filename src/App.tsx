2aa3cbaf398bcab1dc974cd01d9cdbfc6073ce57
Wilder Cossio <w.cossio.r@gmail.com>
2024-03-12 16:45:37 -0400

    installing moment to show a clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import './App.css'; 
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import video from './assets/video.mp4';
+import moment from 'moment';
+import 'moment/locale/es';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState<string>(moment().format('LTS'));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(moment().format('LTS'));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-     <div className='banner'>
-      <video src={video} autoPlay loop muted></video>
-     </div>
+      <div className='video-container'>
+        <video src={video} autoPlay loop muted></video>
+      </div>
       <h1 className='logo'>TimeTracker</h1>
+      <p>{currentTime}</p>
     </div>
   );
 }
