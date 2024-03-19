a4294544be3222feac5ec8fbcd275283e06b7205
Wilder Cossio <w.cossio.r@gmail.com>
2024-03-18 21:27:22 -0400

    making a dropdown menu

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from 'react'
 import video from '../../assets/video.mp4';
 import logo from '../../assets/logo.svg';
 import menu from '../../assets/menu.svg';
+import close from '../../assets/close.svg';
 import './Banner.css';
 import Clock from '../Clock/Clock';
 import CurrentDate from '../CurrentDate/CurrentDate';
 
-
 const Banner = () => {
+
+    const [activeMenu, setActiveMenu] = useState('');
+
+    const showMenu = () => {
+        setActiveMenu('show-menu');
+    }
+
+    const hideMenu = () => {
+        setActiveMenu('hide-menu');
+    }
+
 
     return (
         <div className='banner'>
@@ -15,7 +26,14 @@ const Banner = () => {
                 <video src={video} autoPlay loop muted></video>
             </div>
             <img className='logo' src={logo} alt="logo" />
-            <img className='menu' src={menu} alt="menu" />
+            <img className='menu-icon' src={menu} alt="menu" onClick={showMenu} />
+            <div className={`menu-container ${activeMenu}`}>
+                <div className='menu'>
+                    <img className='close' src={close} alt="close-menu" onClick={hideMenu} />
+                    <p className="menu-options">Sign in</p>
+                    <p className="menu-options">Show Tracks</p>
+                </div>
+            </div>
             <div className="options">
                 <a className='show-tracks' href="#">Show Tracks</a>
                 <button className='btn btn-signin'>SIGN-IN</button>
@@ -23,7 +41,7 @@ const Banner = () => {
             <div className='date-time-container'>
                 <Clock />
                 <CurrentDate />
-            </div>
+                d</div>
             <div className='btn-container'>
                 <button className='btn'>CHECK-IN</button>
                 <button className='btn'>CHECK-OUT</button>
