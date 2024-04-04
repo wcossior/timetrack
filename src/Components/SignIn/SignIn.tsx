ce17d54a27f285620f0ac1c96ab023438a809167
Wilder Cossio <w.cossio.r@gmail.com>
2024-04-04 16:37:11 -0400

    sending the signin form to luxand cloud

--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './SignIn.css';
 import close from '../../assets/close.svg';
 import camera from '../../assets/camara.svg';
+import loading from '../../assets/loading.svg';
 import { useDispatch } from 'react-redux';
 import { hideForm } from '../../redux/slices/SignInSlice';
+import { v4 as uuidv4 } from 'uuid';
 
 const SignIn = () => {
-
+    const API_TOKEN = "353caa8026d941f8834cb0de010c0745";
     const videoRef = useRef<HTMLVideoElement>(null);
     const imgRef = useRef<HTMLImageElement>(null);
     const dispatch = useDispatch();
-    const [cameraState, cameraSetState] = useState<boolean>(false);
+    const [fullName, setFullName] = useState<string>("");
 
     useEffect(() => {
         startVideo();
@@ -42,39 +44,86 @@ const SignIn = () => {
             if (videoRef.current) {
                 videoRef.current.srcObject = stream;
             }
-            cameraSetState(true);
         } catch (err) {
             console.error('Error when opening the camera:', err);
         }
     };
 
+    const captureFullName = (event: ChangeEvent<HTMLInputElement>) => {
+        setFullName(event.target.value);
+    }
+
+    const generateRandomName = (extension: string): string => {
+        const timestamp = Date.now(); // 
+        const uuid = uuidv4(); 
+
+        return `${timestamp}_${uuid}.${extension}`;
+    }
+
+    const sendForm = async () => {
+        if (!imgRef.current) {
+            console.error('There is no image selected');
+            return;
+        }
+
+        const imagePath = imgRef.current.src;
+
+        const response = await fetch(imagePath);
+        const arrayBuffer = await response.arrayBuffer();
+
+        const blob = new Blob([arrayBuffer]);
+
+        const file = new File([blob], generateRandomName("jpeg"), { type: 'image/jpeg' });
+
+        const formData = new FormData();
+        formData.append("photos", file);
+        formData.append("name", fullName);
+        formData.append("store", "1");
+        formData.append("collections", "employeesTrack");
+
+        const requestOptions: RequestInit = {
+            method: 'POST',
+            headers: new Headers({
+                "token": API_TOKEN
+            }),
+            body: formData,
+            redirect: 'follow'
+        };
+
+        try {
+            const response = await fetch("https://api.luxand.cloud/v2/person", requestOptions);
+            const data = await response.json();
+            console.log('Respuesta de Luxand API:', data);
+        } catch (error) {
+            console.error('Error al enviar la imagen a la API de Luxand:', error);
+        }
+    };
+
     return (
-        cameraState ?
 
-            <div className='signin-container'>
-                <div className="form">
-                    <h1>Sign In</h1>
-                    <label>Take a selfie: </label>
-                    <div className="selfie-container">
-                        <video ref={videoRef} autoPlay muted></video>
-                        <button className='btn btn-snapshot' onClick={takeSnapshot}>
-                            <img className='camera' src={camera} />
-                        </button>
-                        <img className='photo-signin' ref={imgRef} />
-                    </div>
-
-                    <div className="name-field-container">
-                        <label htmlFor="fullName">Full Name: </label>
-                        <input type="text" name="fullName" maxLength={50}/>
-                    </div>
-                    <img className='close-form' src={close} alt="close-form" onClick={quitForm} />
-                    <button className='btn btn-send' onClick={takeSnapshot}>
-                        Send
+        <div className='signin-container'>
+            <div className="form">
+                <h1>Sign In</h1>
+                <label>Take a selfie: </label>
+                <div className="selfie-container">
+                    <video ref={videoRef} autoPlay muted></video>
+                    <button className='btn btn-snapshot' onClick={takeSnapshot}>
+                        <img className='camera' src={camera} />
                     </button>
+                    <img className='photo-signin' ref={imgRef} />
                 </div>
+
+                <div className="name-field-container">
+                    <label htmlFor="fullName">Full Name: </label>
+                    <input type="text" name="fullName" value={fullName} onChange={captureFullName} maxLength={50} />
+                </div>
+                <img className='close-form' src={close} alt="close-form" onClick={quitForm} />
+                <button className='btn btn-send' onClick={sendForm}>
+                    Send
+                </button>
             </div>
-            :
-            <div className="loading">Hola cargando</div>
+        </div>
+
     )
 }
 
